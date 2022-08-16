b0f64a5f503f684d692804b8dcb33159f5ff289e
chardwillian <69554100+chardwillian@users.noreply.github.com>
2022-08-16 16:34:37 -0300

    comment form

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,9 @@ export function App() {
    <div className={styles.wrapper}>
     <Sidebar />
       <main>
-        <Post 
-        author="Chard Willian" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, temporibus ipsam amet, tempora unde assumenda voluptatum quis, accusamus odio rem vel id officiis maiores est. Illo sed sapiente alias cumque?"
-        />
-
-        <Post
-        author="Luciana"
-        content="Um novo post bem legal"
-        />
+        <Post/>
+        <Post/>
+        <Post/>
       </main>
    </div>
    </div>
