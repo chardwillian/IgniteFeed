99e33599601199fa8f1fe02f2e34424ae467cd61
chardwillian <69554100+chardwillian@users.noreply.github.com>
2022-08-17 10:01:29 -0300

    avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,12 +7,9 @@ export function Post() {
         <article className={styles.post}>
           <header>
               <div className={styles.author}>
-                <img  
-                className={styles.avatar}
-                src="https://github.com/chardwillian.png"
-                />
+                 <Avatar src="https://github.com/maykbrito.png" />
                 <div className={styles.authorInfo}>
-                    <strong>Chard Willian</strong>
+                    <strong>Mayk Brito</strong>
                     <span>Web Developer</span>
                 </div>
               </div>
